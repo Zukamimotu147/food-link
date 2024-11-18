4e4cc474fb0f0dff97d16d52e7e916c73a0d84da
Zukamimotu147 <marcjacoblasam123@gmail.com>
2024-11-18 17:01:53 +0800

    refactor(auth): remove response message

--- server/src/routes/authRoute.ts
+++ server/src/routes/authRoute.ts
@@ -25,7 +25,7 @@ router.get(
   '/google/callback',
   passport.authenticate('google', { failureRedirect: '/' }),
   (req, res) => {
-    res.redirect('/'); // Redirect to your route after successful login
+    res.redirect('/');
   }
 );
 
