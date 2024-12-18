518240b503ccac0fe0b1550832993769aba6d2cf
Zuka <you@example.com>
2024-12-18 13:42:55 +0800

    some changes

--- client/src/pages/admin1/AdminDashboard.tsx
+++ client/src/pages/admin1/AdminDashboard.tsx
@@ -2,25 +2,23 @@ import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/s
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 import { Separator } from '@/components/ui/separator';
-import ResSidebar from '@/features/restaurant/components/ResSidebar';
+import AdmSidebar from '@/features/admin/components/AdmSidebar';
 import { Outlet } from 'react-router-dom';
-
 gsap.registerPlugin(useGSAP);
-
-const resDashboard = () => {
+const AdminDashboard = () => {
   useGSAP(() => {
     gsap.to('.contentText', {
       duration: 2,
       repeat: -1,
       repeatDelay: 1,
       yoyo: true,
-      text: 'Welcome to Food Link Dashboard',
+      text: 'Welcome to Food Link Admin Dashboard',
     });
   });
   return (
     <main>
       <SidebarProvider>
-        <ResSidebar />
+        <AdmSidebar />
         <SidebarInset>
           <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
             <div className="flex items-center gap-2 px-4">
@@ -38,4 +36,4 @@ const resDashboard = () => {
   );
 };
 
-export default resDashboard;
+export default AdminDashboard;
--- client/src/pages/restaurant1/ResDashboard.tsx
+++ client/src/pages/restaurant1/ResDashboard.tsx
@@ -2,23 +2,25 @@ import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/s
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 import { Separator } from '@/components/ui/separator';
-import AdmSidebar from '@/features/admin/components/AdmSidebar';
+import ResSidebar from '@/features/restaurant/components/ResSidebar';
 import { Outlet } from 'react-router-dom';
+
 gsap.registerPlugin(useGSAP);
-const adminDashboard = () => {
+
+const ResDashboard = () => {
   useGSAP(() => {
     gsap.to('.contentText', {
       duration: 2,
       repeat: -1,
       repeatDelay: 1,
       yoyo: true,
-      text: 'Welcome to Food Link Admin Dashboard',
+      text: 'Welcome to Food Link Dashboard',
     });
   });
   return (
     <main>
       <SidebarProvider>
-        <AdmSidebar />
+        <ResSidebar />
         <SidebarInset>
           <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
             <div className="flex items-center gap-2 px-4">
@@ -36,4 +38,4 @@ const adminDashboard = () => {
   );
 };
 
-export default adminDashboard;
+export default ResDashboard;
